ed2c2c4e209420ede47e800cac6e69ef3f07b3ff
Rogério Maia <rogeriomql@gmail.com>
2020-11-26 17:48:07 -0300

    novos endpoints monitorados

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module monitor-plp
 
 go 1.15
 
-require github.com/RogerioML/plp v1.0.10
+require github.com/RogerioML/plp v1.1.2
--- main.go
+++ main.go
@@ -17,17 +17,18 @@ const (
 
 //Config contem as configurações globais do app
 type Config struct {
-	Dsn       string `json:"dsn"`
-	Intervalo int    `json:"intervalo"`
-	Wsdl      string `json:"wsdl"`
-	MaxConn   int    `json:"conexoes"`
-	Contrato  string `json:"contrato"`
-	Cartao    string `json:"cartao"`
-	Servico   string `json:"servico"`
-	Cnpj      string `json:"cnpj"`
-	Qtd       int    `json:"qtdEtiquetas"`
-	User      string `json:"usuario"`
-	Pass      string `json:"senha"`
+	Dsn          string `json:"dsn"`
+	Intervalo    int    `json:"intervalo"`
+	Wsdl         string `json:"wsdl"`
+	MaxConn      int    `json:"conexoes"`
+	Contrato     string `json:"contrato"`
+	Cartao       string `json:"cartao"`
+	Servico      string `json:"servico"`
+	Cnpj         string `json:"cnpj"`
+	Qtd          int    `json:"qtdEtiquetas"`
+	User         string `json:"usuario"`
+	Pass         string `json:"senha"`
+	IDPlpCliente string `json:"idPlpCliente"`
 }
 
 func main() {
@@ -42,6 +43,7 @@ func main() {
 	if err = jsonParser.Decode(&config); err != nil {
 		log.Fatal(err.Error())
 	}
+	plp.Wsdl = config.Wsdl
 
 	timer := time.NewTicker(time.Duration(config.Intervalo) * time.Second)
 	for {
@@ -50,14 +52,26 @@ func main() {
 			//solicita uma faixa de etiquetas
 			now := time.Now()
 
-			servicos, err := plp.BuscaServicos(config.Wsdl, "9912408500", "0072922621", config.User, config.Pass)
+			endereco, err := plp.ConsultaCEP("71917360")
+			if err != nil {
+				log.Printf("falha ao consultar cep: %s", err.Error())
+				continue
+			}
+			log.Printf("buscaCEP %.3f endereco obtido: %s %s %s - %s", time.Since(now).Seconds(),
+				endereco.Body.ConsultaCEPResponse.Return.Endereco,
+				endereco.Body.ConsultaCEPResponse.Return.Bairro,
+				endereco.Body.ConsultaCEPResponse.Return.Cidade,
+				endereco.Body.ConsultaCEPResponse.Return.UF,
+			)
+
+			servicos, err := plp.BuscaServicos("9912408500", "0072922621", config.User, config.Pass)
 			if err != nil {
 				log.Printf("falha ao consultar cliente: %s: ", err.Error())
 				continue
 			}
 			log.Printf("buscaServicos %.3f total de %d servicos obtidos", time.Since(now).Seconds(), len(servicos.Body.BuscaServicosResponse.Return))
 
-			faixa, err := plp.SolicitaEtiquetas(config.Wsdl, config.Servico, config.Cnpj, config.Qtd, config.User, config.Pass)
+			faixa, err := plp.SolicitaEtiquetas(config.Servico, config.Cnpj, config.Qtd, config.User, config.Pass)
 			if err != nil {
 				log.Printf("falha ao obter etiquetas: %s", err.Error())
 				continue
@@ -70,7 +84,7 @@ func main() {
 			//solicita o digito verificador para  faixa gerada
 			now = time.Now()
 			etq := faixa[:13]
-			verificador, err := plp.GeraDigitoVerificadorEtiquetas(config.Wsdl, etq)
+			verificador, err := plp.GeraDigitoVerificadorEtiquetas(etq)
 			if err != nil {
 				log.Printf("falha ao obter digito verificador: %s", err.Error())
 				continue
@@ -81,12 +95,19 @@ func main() {
 			//gerar a plp com a etiqueta obtida
 			etqSemVerificador := strings.Replace(etq, " ", "", -1)
 			now = time.Now()
-			plpNu, err := plp.FechaPlpVariosServicos(config.Wsdl, etqComVerificador, etqSemVerificador)
+			plpNu, err := plp.FechaPlpVariosServicos(etqComVerificador, etqSemVerificador, config.IDPlpCliente, config.Cartao, config.User, config.Pass)
 			if err != nil {
 				log.Println("erro ao fechar PLP: " + err.Error())
 				continue
 			}
 			log.Printf("fechaPlpVariosServicos %.3f plp obtida: %s", time.Since(now).Seconds(), plpNu)
+
+			_, err = plp.SolicitaPLP(plpNu, etqComVerificador, config.User, config.Pass)
+			if err != nil {
+				log.Println("erro ao obter PLP: " + err.Error())
+				continue
+			}
+			log.Printf("solicitaPLP %.3f plp obtida com sucesso", time.Since(now).Seconds())
 		}
 	}
 }
